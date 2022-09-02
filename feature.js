aca6a59eb7e245c0e3716bb2068bfba757347129
twoooooda <yuuyatsuda@yahoo.co.jp>
2022-09-02 18:44:44 +0900

    edit feature.js

--- feature.js
+++ feature.js
@@ -9,3 +9,19 @@ function showClock1() {
 
 setInterval('showClock1()', 1000);
 
+// let url = 'https://www.train-guide.westjr.co.jp/api/v3/area_kinki_trafficinfo.json';
+
+// async function callApi(url) {
+//     const res = await fetch(url, {method: 'GET',headers: {'Content-Type': 'application/json'}});
+//     const users = await res.json();
+//     console.log(users)
+//   };
+  
+//   callApi(url);
+// noticeDelay()
+
+// async function noticeDelay(){
+//     const res = await fetch("https://www.train-guide.westjr.co.jp/api/v3/area_kinki_trafficinfo.json");
+//     document.getElementById("DelayInfo").innerHTML = res.text();
+//     console.log(res);
+// }
