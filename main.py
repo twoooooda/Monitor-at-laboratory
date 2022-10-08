ae7b8c342c90cf9c99a73fc725e16e3100dc4b86
twoooooda <yuuyatsuda0602@gmail.com>
2022-10-08 11:10:41 +0900

    fix main.py

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ date = datetime.datetime.today().strftime("%Y/%m/%d")
 res = requests.get(url="http://api.jugemkey.jp/api/horoscope/free/" + date)
 
 if str(res.raise_for_status()) == "None":
-    with open("./json_fortune.josn", "w") as f:
-        json.dumps(json.loads(res.text), f)
+    results = json.loads(res.text)
+    with open("json_fortune.json", "w", encoding='utf-8') as f:
+        json.dump(results, f)
 else:
-    pass
+    results = ""
+    with open("json_fortune.json", "w", encoding='utf-8') as f:
+        json.dump(results, f)
     # print(json.dumps(json.loads(res.text), indent=4, ensure_ascii=False))
     # #たとえば、牡羊座のみ取得したい場合
     # print(res.json()["horoscope"][date][0])
