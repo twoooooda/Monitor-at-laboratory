25ddc2c80e8a66888ca845f3bff21f194e4bd3aa
twoooooda <yuuyatsuda0602@gmail.com>
2023-04-25 10:57:29 +0900

    update API URL

--- getAPI/main.py
+++ getAPI/main.py
@@ -5,8 +5,9 @@ import datetime
 date = datetime.datetime.today().strftime("%Y/%m/%d")
 
 # 占いを叩いてた時 http://api.jugemkey.jp/api/horoscope/year/month/day の形式
-#掃除当番APIを叩く
-res = requests.get(url="https://script.google.com/macros/s/AKfycbzRi4QqDpCxY1inNFO8EFLxaI8l0616qny_M8EGPTQqV22PJpz4yQfsY6nBQ-Uepr-W/exec")
+# 掃除当番APIを叩く
+res = requests.get(
+    url="https://script.google.com/macros/s/AKfycbzNuWBjvZ4ahHHR_TiEaS8JIdZqKFI38ToSc5-f6Nzpxwn-bsxDrEV7GeGmsVcEs3pZ/exec")
 
 if str(res.raise_for_status()) == "None":
     results = json.loads(res.text)
@@ -14,7 +15,7 @@ if str(res.raise_for_status()) == "None":
     with open("json_fortune.json", "w", encoding='utf-8') as f:
         json.dump(results, f, ensure_ascii=False)
 
-    #現在時刻を書き込むことで強制的に差分を生ませる
+    # 現在時刻を書き込むことで強制的に差分を生ませる
     f = open("nowtime.txt", "w")
     f.write(datetime.datetime.now().strftime("%Y/%m/%d/%H:%M:%S"))
     f.close()
